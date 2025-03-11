41f34358cbffe58206088a781b8d601b2bac9312
Jad Menkara <Jadmenkara@gmail.com>
2025-03-11 18:17:37 -0400

    Update Details.jsx

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -34,7 +34,8 @@ function Details() {
                         <div className ="Faq">
                             <FontAwesomeIcon icon={faWrench} className="FaqIcon" />
                             Who can attend BotBuilders?
-                            <p></p>
+
+                            <p className = "FaqAnswer">BotBuilder's curriculum is designed for older students grades 7-8.  All students must complete an application form.</p>
                         </div>
                     </div>
 
@@ -43,12 +44,12 @@ function Details() {
                             <div className ="Faq">
                                 <FontAwesomeIcon icon={faWrench} className="FaqIcon" /> 
                                 How much does the program cost?
-                                <p></p>
+                                <p className = "FaqAnswer">BotBuilders is completely free for all students.</p>
                             </div>
                             <div className ="Faq">
                                 <FontAwesomeIcon icon={faWrench} className="FaqIcon" />
                                 What will I get from BotBuilders?
-                                <p></p>
+                                <p className = "FaqAnswer">You get to bring home a programmable robot car alongside a lifelong love of robotics!</p>
                             </div>
                     </div>
 
@@ -58,24 +59,24 @@ function Details() {
                         <div className ="Faq">
                             <FontAwesomeIcon icon={faWrench} className="FaqIcon" />
                             When/Where is the program held?
-                            <p></p>
+                            <p className = "FaqAnswer">BotBuilders is held at Vincent Massey Secondary School, located at 1800 Liberty St, Windsor, ON. It takes place from 3:40 to 4:45 every Wednesday, starting April 9th and ending May 28th.</p>
                         </div>
                         <div className ="Faq">
                                 <FontAwesomeIcon icon={faWrench} className="FaqIcon" />
                                 Who teaches BotBuilders?
-                                <p></p>
+                                <p className = "FaqAnswer">BotBuilders is taught by Jad Menkara and Kevin Dang, ambitious high school engineers looking to jumpstart a passion for engineering in the next generation. For more details, please navigate to the About section.</p>
                             </div>
                     </div>
                     <div style ={{display:"flex"}}>
                         <div className ="Faq">
                             <FontAwesomeIcon icon={faWrench} className="FaqIcon" /> 
                             When are applications due?
-                            <p></p>
+                            <p className = "FaqAnswer">Applications are due Wednesday, April 2nd, 2025.</p>
                         </div>
                         <div className ="Faq">
                             <FontAwesomeIcon icon={faWrench} className="FaqIcon" />
                             Is this program approved?
-                            <p></p>
+                            <p className = "FaqAnswer">BotBuilders has been approved by the Greater Essex District School Board. It is supervised by Ms. Beiko, a technology teacher at Vincent Massey Secondary School. </p>
                         </div>
                     </div>
                         
